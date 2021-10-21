8e31babb997e96a8b9d51516f5c16bf5e42515d6
temmmus <temmmus@gmail.com>
2021-10-22 01:34:30 +0500

    дописана вся логика открытия/закрытия попапа и изменения профиля

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
 let editButton = document.querySelector(".profile__edit-button");
 let closeButton = document.querySelector(".popup__close-button");
 let saveButton = document.querySelector(".popup__save-button");
-let profileName= document.querySelector(".profile__name");
-let profileOccupation = document.querySelector(".profile__occupation");
-let popupProfileName = document.querySelector(".popup__input_name");
-let popupProfileOccupation= document.querySelector(".popup__input_occupation");
 let popup = document.querySelector(".popup");
-let likeCheckbox = document.querySelector(".place__like-checkbox");
-let likeLable = document.querySelector(".place__like-lable");
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let inputName = document.querySelector('.popup__input_name');
+let inputAbout = document.querySelector('.popup__input_about');
 
 
 
 // открытие попапа
-editButton.addEventListener('click', function () {
+function openPopup() {
+    inputName.value = profileName.textContent
+    inputAbout.value = profileAbout.textContent
     popup.classList.add('popup_opened');
-  }); 
+};
+editButton.addEventListener('click', openPopup);
+
+
 
 // закрытие попапа
-closeButton.addEventListener('click', function () {
+function closePopup() {
+    popup.classList.toggle('popup_opened'); 
+};
+closeButton.addEventListener('click', closePopup);
+
+
+
+
+// обновление профиля
+function changeProfile() {
+    profileName.textContent = inputName.value
+    profileAbout.textContent = inputAbout.value
     popup.classList.toggle('popup_opened');
-  });   
+};
+saveButton.addEventListener('click', changeProfile);
 
+document.addEventListener('keypress', function(event) {
+    if (popup.classList.contains('popup_opened') && event.key === 'Enter') {
+        changeProfile()
+  }
+});
 
-saveButton.addEventListener('click', function () {
-    profileName.textContent = popupProfileName.textContent;
-    popup.classList.toggle('popup_opened');
-    return profileName.textContent;
-  });     
-
-
-
-// проставление лайка
-// likeCheckbox.addEventListener('change', function () {
-//     likeLable.classList.toggle('place__like-lable_checked');
-//   });   
+document.addEventListener('keyup', function(event) {
+    if (popup.classList.contains('popup_opened') && event.key === 'Escape') {
+        closePopup();
+  }
+});
